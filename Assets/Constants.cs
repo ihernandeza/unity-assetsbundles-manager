1f64cad08e0a04d1c3cf58f2433112f077923f79
Italo Hernández <iherar@gmail.com>
2018-04-02 15:57:57 -0300

    first commit

---
+++ Assets/Constants.cs
@@ -0,0 +1,7 @@
+﻿public class Constants  {
+
+    /// <summary>
+    /// URL de la carpeta que contiene los AssetsBundles
+    /// </summary>
+    public const string urlEndpointAB = "http://www.ihernandez.cl/ab_test/";
+}
